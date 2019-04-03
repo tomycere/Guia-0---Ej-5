1fea8548fb8324664595898e91bdda7f2119401e
tomas.a.cerezo@outlook.com <tomas.a.cerezo@outlook.com>
2019-04-03 18:11:35 -0300

    Guia 0 - Ejercicio 5

--- Guia 0 - Ej 5/Program.cs
+++ Guia 0 - Ej 5/Program.cs
@@ -10,6 +10,75 @@ namespace Guia_0___Ej_5
     {
         static void Main(string[] args)
         {
+            Console.Write("Ingrese un numero entero mayor a 0:");
+            string ingreso = Console.ReadLine();
+            int numero;
+            Console.WriteLine();
+
+            if (int.TryParse(ingreso, out numero))
+            {
+                for (int i = 0; i <= numero; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine("2)");
+
+            Console.Write($"Ingrese un numero entero menor a {numero} y mayor a 0 :");
+            string ingreso2 = Console.ReadLine();
+            
+            Console.WriteLine();
+
+            if (int.TryParse(ingreso, out int numero2) && (numero2<numero))
+            {
+                for (int i = 0; i <= numero; i += numero2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine("3)");
+
+            int indice = 0;
+            do
+            {
+                if (int.TryParse(ingreso, out numero))
+                {
+                    indice++;
+                    Console.WriteLine(indice);
+                    
+                }
+            } while (indice<numero);
+
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine("3.1)");
+
+            int indice2 = 0;
+            while (indice2<numero)
+            {
+                if(int.TryParse(ingreso, out numero))
+                {
+                    indice2++;
+                    Console.WriteLine(indice2);
+
+                }
+            }
+
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine("4)");
+
+
+
+
+
         }
+
     }
 }
